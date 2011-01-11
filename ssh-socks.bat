5fa04a0ef0805137ab3abb44bfcbd6ba2c39b076
Brandon Siegel <brandon@smartercode.net>
2011-01-11 16:38:17 -0500

    Add auto proxy configuration for proxyscript

--- ssh-socks.bat
+++ ssh-socks.bat
@@ -1,1 +1,6 @@
-ssh -N -D 9050 -i "p:\keys\volatile.id_rsa" bes7@129.22.150.118
+@echo off
+setproxy ;socks=127.0.0.1:9050
+start /B ssh -N -D 9050 -i "p:\keys\volatile.id_rsa" bes7@129.22.150.118
+pause
+setproxy none
+taskkill /F /IM ssh.exe
